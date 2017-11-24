4b2d18033d1c0fc76fb44b949b010f525e1ec567
ShiranuiNui <shiranuisayaka@gmail.com>
2017-11-24 22:14:22 +0900

    [Fix] Models Fixed for New Specification for #4

--- src/Models/Teacher.cs
+++ src/Models/Teacher.cs
@@ -8,11 +8,13 @@ namespace FUNAPI.Models
     {
         public int TeacherId { get; set; }
         public string disp_teacher { get; set; }
+        public string roman_name { get; set; }
+        public string position { get; set; }
         public string research_area { get; set; }
-        public int course { get; set; }
+        public string role { get; set; }
         [JsonIgnore]
         public List<LectureTeacher> LectureTeachers { get; set; } = new List<LectureTeacher>();
         public override string ToString() =>
-            $"<Teacher Class> id={this.TeacherId}, disp={this.disp_teacher}, area={this.research_area}, course={this.course}, LectureCount={this.LectureTeachers.Count}";
+            $"<Teacher Class> id={this.TeacherId}, disp={this.disp_teacher}, roman={this.roman_name}, potision={this.position}, research_area={this.research_area}, role={this.role}, LectureCount={this.LectureTeachers.Count}";
     }
 }
